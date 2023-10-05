4938abba58df8452af8d7a52b0f36147065399b8
Gatsbeau <jabriel0ne@gmail.com>
2023-10-04 20:43:41 -0400

    General edits

--- assets/js/favorite.js
+++ assets/js/favorite.js
@@ -4,13 +4,17 @@ function displayFav() {
   let favGalleryString = localStorage.getItem("favorites");
   let favGallery = JSON.parse(favGalleryString);
   console.log(favGallery);
-  for (let i = 0; i < favGallery.length; i++) {
+  let filteredGallery = favGallery.filter((value, index, self) => {
+    return self.indexOf(value) === index;
+  });
+  console.log(u); // Output: [1, 2, 3, 4, 5]
+  for (let i = 0; i < filteredGallery.length; i++) {
     var columnNum = i % 4;
     var newCard = document.createElement("figure");
     newCard.classList.add("image", "is-3by4", "pokeCard");
         newCard.innerHTML =
           '<img src="' +
-          favGallery[i] +
+          filteredGallery[i] +
           '" /><span class="favoriteBtn">❤</span>';
         columns[columnNum].appendChild(newCard);
   }
